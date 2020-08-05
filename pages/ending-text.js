ac98051398fff48f9b058f45a8b0075decd700aa
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-08-05 14:41:49 -0600

    Add ending text.

---
+++ pages/ending-text.js
@@ -0,0 +1,22 @@
+export default function Background() {
+	return (
+		<>
+			<h1 className="starting">Stream Ending</h1>
+
+			<style jsx>{`
+				.starting {
+					background-image: linear-gradient(to left, var(--pink), var(--green));
+					background-clip: text;
+					color: transparent;
+					display: inline-block;
+					-webkit-background-clip: text;
+					text-fill-color: transparent;
+					-webkit-fill-color: transparent;
+					font-family: var(--ff);
+					font-size: 4rem;
+					margin: 0;
+				}
+			`}</style>
+		</>
+	)
+}
